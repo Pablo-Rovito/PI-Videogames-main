a133a151d452a89b6b58742979396b40cc79876a
Pablo-Rovito <pablo.rovito@outlook.com>
2022-03-20 02:50:44 -0300

    cors +++

--- api/src/app.js
+++ api/src/app.js
@@ -9,7 +9,12 @@ const cors = require('cors');
 
 server.name = 'API';
 
-server.use(cors());
+server.use(
+	cors({
+		origin: 'https://pi-videogames-main-hazel.vercel.app/',
+		credentials: true,
+	})
+);
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
